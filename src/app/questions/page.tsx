618ff8b70aa070752fc55ad19fc4758ffcfc0999
Abhi <silwalabhi2@gmail.com>
2025-07-02 16:36:21 +0545

    added new features

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -6,9 +6,9 @@ export default function Questions()
 {
     return (
         <div className="grid place-items-center"> 
-            <div className="grid items-center justify-center text-center mt-32 bg-red-900">
+            <div className="grid items-center justify-center text-center mt-32 bg-gradient-to-bl from-pink-400 via-red-500 via  to-purple-900">
             {pages.map((item)=>
-                <Link href={`/questions/${item}`} className="p-3 ">{item}</Link>
+                <Link href={`/questions/${item}`} className="p-3 m-2 backdrop-blur-xl shadow-sm shadow-white bg-yellow-200/20 rounded-3xl">{item}</Link>
             )}
             </div>
         </div>
