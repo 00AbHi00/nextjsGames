d5c256a588b2d160087aca6b7c95263673b9fcd2
Abhi <silwalabhi2@gmail.com>
2025-07-11 08:53:26 +0545

    fixed things that caused issues

--- src/app/questions/fetch/page.tsx
+++ src/app/questions/fetch/page.tsx
@@ -1,19 +1,19 @@
-export async function getServerSideProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
-  .then(()=>
-  {
-    console.log()
-  }).catch(()=>
-  {
-    console.error("errr")
-  });
-  const data = await res.json();
+// export async function getServerSideProps() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
+//   .then(()=>
+//   {
+//     console.log()
+//   }).catch(()=>
+//   {
+//     console.error("errr")
+//   });
+//   const data = await res.json();
 
-  return {
-    props: { user: data },
-  };
-}
+//   return {
+//     props: { user: data },
+//   };
+// }
 
-export default function Profile({ user }) {
-  return <h1>{user.name}</h1>;
-}
+// export default function Profile({ user }) {
+//   return <h1>{user.name}</h1>;
+// }
